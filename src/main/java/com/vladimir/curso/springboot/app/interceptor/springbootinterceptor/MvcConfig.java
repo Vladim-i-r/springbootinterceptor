615284e447ec7e213450d0b1e2f6c141fabb54ac
Vladim-i-r <vladimir.luna96@hotmail.com>
2024-03-02 21:09:33 -0600

    first commit

---
+++ src/main/java/com/vladimir/curso/springboot/app/interceptor/springbootinterceptor/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.vladimir.curso.springboot.app.interceptor.springbootinterceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer{             //? Con esto se registra el interceptor
+
+    @Autowired
+    @Qualifier("loadingTimeInterceptor")
+    private HandlerInterceptor timeInterceptor;                 // Agarramos el mas generico pero puede ser el LoadingTimeInterceptor
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(timeInterceptor);                //.addPathPatterns();   .excludePathPatterns()
+    }
+
+    
+}
